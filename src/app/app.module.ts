287168f07e092a33e52105b48989fc472d863d40
anson <message9698@gmail.com>
2024-06-18 01:18:57 +0800

    install youtube api. It is bad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { YouTubePlayerModule } from '@angular/youtube-player';
+import { AnimateTriggerComponent } from './animate-trigger/animate-trigger.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoFiComponent } from './lo-fi/lo-fi.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AnimateTriggerComponent } from './animate-trigger/animate-trigger.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { YeahComponent } from './yeah/yeah.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoFiComponent,
-    AnimateTriggerComponent
+    AnimateTriggerComponent,
+    YeahComponent,
   ],
   imports: [
     BrowserModule,
@@ -21,7 +24,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    YouTubePlayerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/yeah/yeah.component.ts
+++ src/app/yeah/yeah.component.ts
@@ -1,14 +1,29 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { YouTubePlayer } from '@angular/youtube-player';
 
 @Component({
   selector: 'app-yeah',
-  standalone: true,
-  imports: [
-    CommonModule,
-  ],
   templateUrl: './yeah.component.html',
-  styleUrls: ['./yeah.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./yeah.component.css']
 })
-export class YeahComponent { }
+export class YeahComponent implements OnInit {
+
+  readonly vedioIds: string[] = [
+    'NajJFMnGMf0',
+    'PJH2AscfsNc',
+    'IXdszpdqzec',
+    'SWEyHRr12Jo',
+  ]
+  readonly playerVars: YT.PlayerVars = {
+    autoplay: 1,
+    loop: 0,
+  }
+  playerVarsList: YT.PlayerVars[] = [];
+
+  ngOnInit(): void {
+    this.playerVarsList = this.vedioIds.map(x => ({
+      ...this.playerVars,
+      playlist: x
+    }) as YT.PlayerVars)
+  }
+}
