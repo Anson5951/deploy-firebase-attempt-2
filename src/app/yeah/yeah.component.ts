ef9fcec2db2500a9c51ab231828b9fa19aedf082
anson <message9698@gmail.com>
2024-09-30 01:01:00 +0800

    change arrangement

--- src/app/yeah/yeah.component.ts
+++ src/app/yeah/yeah.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit, HostListener } from '@angular/core';
 import { YouTubePlayer } from '@angular/youtube-player';
 
 @Component({
@@ -7,6 +7,16 @@ import { YouTubePlayer } from '@angular/youtube-player';
     styleUrls: ['./yeah.component.css']
 })
 export class YeahComponent implements OnInit {
+
+    layoutSelectorVisibility = false
+
+    @HostListener('window:keydown', ['$event'])
+    handleKeyDown(event: KeyboardEvent) {
+        console.log('KeyboardEvent',event);
+        if (event.key == '`') {
+            this.layoutSelectorVisibility = !this.layoutSelectorVisibility;
+        }
+    }
 
     // readonly vedioIds: string[] = [
     //     'NajJFMnGMf0',
@@ -25,5 +35,16 @@ export class YeahComponent implements OnInit {
         //     ...this.playerVars,
         //     playlist: x
         // }) as YT.PlayerVars)
+    }
+
+    layoutSelection_onClick(layout: 'quarter' | 'heigh' | 'wide') {
+        switch (layout) {
+            case 'quarter':
+                break;
+            case 'heigh':
+                break;
+            case 'wide':
+                break;
+        }
     }
 }
